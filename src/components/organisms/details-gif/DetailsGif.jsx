3bbcbdc71e40ee3f836860d885b843a9badfdd44
Eduardo <ejrb1234@gmail.com>
2023-10-31 13:00:53 -0400

    added task

--- src/components/organisms/details-gif/DetailsGif.jsx
+++ src/components/organisms/details-gif/DetailsGif.jsx
@@ -19,40 +19,41 @@ export const DetailsGif = () => {
     };
   }, []);
 
+  // TODO: Cambiar este condición a un estado de loading, cuando se cree el customHook para esta petición.
+  if (!gif) {
+    return <Loader />;
+  }
+
   return (
     <>
-      {gif ? (
-        <section className={styles["container"]}>
-          <aside className={styles["aside"]}>
-            <img
-              className={styles["avatar"]}
-              src={gif.user.avatar}
-              alt={`Avatar: ${gif.user.name}`}
-            />
-            <div className={styles["user-info"]}>
-              <h3 className={styles["user-name"]}>
-                {gif.user.name || "Anonymous"}
-              </h3>
-              <p>{gif.user.description || "No description"}</p>
-              {gif.user.is_verified ? (
-                <p>Es verificado</p>
-              ) : (
-                <p>No es verificado</p>
-              )}
-            </div>
-          </aside>
-          <article className={styles["gif-container"]}>
-            <header className={styles["gif-header"]}>
-              <h2 className={styles["gif-title"]}>{gif.title}</h2>
-            </header>
-            <div>
-              <img src={gif.image} alt={gif.title} />
-            </div>
-          </article>
-        </section>
-      ) : (
-        <Loader />
-      )}
+      <section className={styles["container"]}>
+        <aside className={styles["aside"]}>
+          <img
+            className={styles["avatar"]}
+            src={gif.user.avatar}
+            alt={`Avatar: ${gif.user.name}`}
+          />
+          <div className={styles["user-info"]}>
+            <h3 className={styles["user-name"]}>
+              {gif.user.name || "Anonymous"}
+            </h3>
+            <p>{gif.user.description || "No description"}</p>
+            {gif.user.is_verified ? (
+              <p>Es verificado</p>
+            ) : (
+              <p>No es verificado</p>
+            )}
+          </div>
+        </aside>
+        <article className={styles["gif-container"]}>
+          <header className={styles["gif-header"]}>
+            <h2 className={styles["gif-title"]}>{gif.title}</h2>
+          </header>
+          <div>
+            <img src={gif.image} alt={gif.title} />
+          </div>
+        </article>
+      </section>
     </>
   );
 };
