17dba918e4731c68889cb78d6dab9035bd754971
Eduardo <ejrb1234@gmail.com>
2023-10-31 15:06:18 -0400

    details user component + details gif message

--- src/components/organisms/details-gif/DetailsGif.jsx
+++ src/components/organisms/details-gif/DetailsGif.jsx
@@ -5,6 +5,10 @@ import { useParams } from "react-router-dom";
 
 import styles from "./DetailsGif.module.css";
 
+import { DetailsUser } from "@/components/molecules/details-user/DetailsUser";
+import { Info } from "lucide-react";
+import { XCircle } from "lucide-react";
+
 export const DetailsGif = () => {
   let { id } = useParams();
 
@@ -19,6 +23,11 @@ export const DetailsGif = () => {
     };
   }, []);
 
+  const [isOpenDetails, setIsOpenDetails] = useState(false);
+  const handleClick = () => {
+    setIsOpenDetails(!isOpenDetails);
+  };
+
   // TODO: Cambiar este condición a un estado de loading, cuando se cree el customHook para esta petición.
   if (!gif) {
     return <Loader />;
@@ -28,29 +37,46 @@ export const DetailsGif = () => {
     <>
       <section className={styles["container"]}>
         <aside className={styles["aside"]}>
-          <img
-            className={styles["avatar"]}
-            src={gif.user.avatar}
-            alt={`Avatar: ${gif.user.name}`}
-          />
-          <div className={styles["user-info"]}>
-            <h3 className={styles["user-name"]}>
-              {gif.user.name || "Anonymous"}
-            </h3>
-            <p>{gif.user.description || "No description"}</p>
-            {gif.user.is_verified ? (
-              <p>Es verificado</p>
-            ) : (
-              <p>No es verificado</p>
-            )}
-          </div>
+          <DetailsUser user={gif.user} />
         </aside>
         <article className={styles["gif-container"]}>
           <header className={styles["gif-header"]}>
             <h2 className={styles["gif-title"]}>{gif.title}</h2>
+            <button
+              type="button"
+              onClick={handleClick}
+              className={styles["details-gif"]}
+            >
+              {isOpenDetails ? (
+                <XCircle size={18} strokeWidth={2} color="white" />
+              ) : (
+                <Info size={18} strokeWidth={2} color="white" />
+              )}
+            </button>
           </header>
-          <div>
-            <img src={gif.image} alt={gif.title} />
+          <div className={styles["gif-container"]}>
+            {isOpenDetails && (
+              <div className={styles["gif-details"]}>
+                <span className={styles["gif-details-stat"]}>
+                  Source: {gif.details.width}x{gif.details.height}px
+                </span>
+                <span className={styles["gif-details-stat"]}>Size: {}</span>
+                <span className={styles["gif-details-stat"]}>
+                  Frames: {gif.details.frames}
+                </span>
+                <span className={styles["gif-details-stat"]}>
+                  Uploaded: {gif.details.uploaded}
+                </span>
+                <span className={styles["gif-details-stat"]}>
+                  Rating: {gif.details.rating}
+                </span>
+              </div>
+            )}
+            <img
+              className={styles["gif-image"]}
+              src={gif.image}
+              alt={gif.title}
+            />
           </div>
         </article>
       </section>
