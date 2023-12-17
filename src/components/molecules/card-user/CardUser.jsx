eb73781e46f553a16261d40210e4b73dae8f49a8
Eduardo <ejrb1234@gmail.com>
2023-12-16 23:55:49 -0400

    new CategoryTitle with Compound Components

--- src/components/molecules/card-user/CardUser.jsx
+++ src/components/molecules/card-user/CardUser.jsx
@@ -5,19 +5,12 @@ export const CardUser = ({ picture, name, nickname, email }) => {
     <>
       <div className={styles.container}>
         <section className={styles['user-information']}>
-          <img className={styles.picture} src={picture} alt="Profile" />
+          <img className={styles.picture} src={picture} alt="User picture" />
           <div className={styles.content}>
             <p className={styles.name}>{name}</p>
             <p>{nickname}</p>
             <p>{email}</p>
           </div>
-        </section>
-        <section className={styles.favorites}>
-          <div className={styles.header}>
-            <p>My Collections</p>
-            <div className={styles['layout-mode']}></div>
-          </div>
-          <p>0</p>
         </section>
       </div>
     </>
--- src/components/organisms/gifs-grid/GifsGrid.jsx
+++ src/components/organisms/gifs-grid/GifsGrid.jsx
@@ -2,65 +2,37 @@ import ProTypes from 'prop-types'
 
 import styles from './GifsGrid.module.css'
 
-import { CategoryTitle } from '@/components/molecules/category-title/CategoryTitle'
 import { Loader } from '@/components/atoms/loader/Loader'
-import { copyToClipboard } from '@/libs/utils'
-import { Link as LinkIcon, Heart } from 'lucide-react'
-import { Link } from 'react-router-dom'
+import { Gif } from '@/components/atoms/gif/Gif'
+import { CategoryTitle } from '@/components/molecules/category-title/CategoryTitle'
+import { ChevronRight } from 'lucide-react'
+import { rightChevron } from '@/libs/lucide'
 
 export const GifsGrid = ({ category, gifs, loading }) => {
-  const myStorage = window.localStorage
-
-  const handleAddFavourite = ({ id }) => {
-    const favourite = id
-    const favouritesGifs = JSON.parse(myStorage.getItem('favourites'))
-
-    if (myStorage.length === 0) {
-      myStorage.setItem('favourites', JSON.stringify([favourite]))
-    } else {
-      if (favouritesGifs.includes(favourite)) {
-        const filteredGifs = favouritesGifs.filter(gif => gif !== favourite)
-        myStorage.setItem('favourites', JSON.stringify(filteredGifs))
-      } else {
-        favouritesGifs.push(favourite)
-        myStorage.setItem('favourites', JSON.stringify(favouritesGifs))
-      }
-    }
-  }
-  
   return (
     <>
       <div className={styles.container}>
-        <CategoryTitle text={category} />
+        <CategoryTitle>
+          <CategoryTitle.LinkComponent to={`/category/${category}`}>
+            <CategoryTitle.Title>{category}</CategoryTitle.Title>
+          </CategoryTitle.LinkComponent>
+          <CategoryTitle.LinkComponent to={`/category/${category}`}>
+            <CategoryTitle.SecondaryText>
+              All the GIFs
+              <ChevronRight
+                size={rightChevron.size}
+                strokeWidth={rightChevron.strokeWidth}
+              />
+            </CategoryTitle.SecondaryText>
+          </CategoryTitle.LinkComponent>
+        </CategoryTitle>
 
         <div className={styles['gifs-container']}>
           {loading && <Loader />}
           {gifs &&
             gifs.map(({ url, title, id, image }) => {
               return (
-                <div key={id} className={styles['gif-container']}>
-                  <Link to={`/gifs/${id}`}>
-                    <img
-                      className={styles['gif-img']}
-                      src={image}
-                      alt={title}
-                    />
-                  </Link>
-                  <div className={styles['actions-buttons']}>
-                    <button
-                      className={styles['add-favourite-btn']}
-                      onClick={() => handleAddFavourite({ id })}
-                    >
-                      <Heart size={20} />
-                    </button>
-                    <button
-                      className={styles['copy-btn']}
-                      onClick={() => copyToClipboard(url)}
-                    >
-                      <LinkIcon size={20} />
-                    </button>
-                  </div>
-                </div>
+                <Gif key={id} url={url} title={title} id={id} image={image} />
               )
             })}
         </div>
